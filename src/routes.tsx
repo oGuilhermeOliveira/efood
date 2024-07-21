0b2ee34de96d3473c1b3a3796cfb7a12e4ff537b
Guilherme Oliveira dos Santos <guilhermeolsnto@gmail.com>
2024-07-21 13:43:12 -0300

    4

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './pages/home'
 import Menu from './pages/Menu'
 
 const Rotas = () => (
--- src/routs.tsx
+++ src/routs.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './pages/home'
 import Product from './pages/Menu'
 
 const Rotas = () => (
